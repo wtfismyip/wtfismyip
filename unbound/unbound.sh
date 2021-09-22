798ed986e0c98812dbadf46812fe2b874b2e7a84
Clint Ruoho <clint@wtfismyip.com>
2021-09-22 10:43:46 +0000

    Generate unbound.conf on the fly based on number of CPUs

--- unbound/unbound.sh
+++ unbound/unbound.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 /usr/lib/unbound/package-helper chroot_setup
 /usr/lib/unbound/package-helper root_trust_anchor_update
+CPUS=`lscpu | grep ^CPU\(s | cut -d : -f2 | sed "s/^ *//g"`
+sed -i "s/CPUS/$CPUS/g" /etc/unbound/unbound.conf
 /usr/sbin/unbound -d
